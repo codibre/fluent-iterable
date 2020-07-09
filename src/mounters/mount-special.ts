34d4c8912bcf015c4c1cf5774ba873ec41e7cb4d
Thiago Santos <thiago.santos@maxmilhas.com.br>
2020-07-09 15:54:02 -0300

    fixing incorrect mapping of groupAsync to AsyncIterable

--- src/mounters/mount-special.ts
+++ src/mounters/mount-special.ts
@@ -10,12 +10,18 @@ export function mountSpecial<T>(
   wrapper: (...args: any[]) => any,
   asyncWrapper: (...args: any[]) => any,
 ) {
-  return {
-    group: (...args: any[]) =>
-      wrapper(group(iterable, ...args)).map(fluentGroup),
-    groupAsync: (...args: any[]) =>
-      asyncWrapper(groupAsync(iterable, ...args)).map(fluentGroup as any),
-    partition: (...args: any[]) =>
-      wrapper(partition(iterable, ...args)).map((part: any) => wrapper(part)),
-  };
+  const result = {} as any;
+  if (group) {
+    result.group = (...args: any[]) =>
+      wrapper(group(iterable, ...args)).map(fluentGroup);
+  }
+  if (groupAsync) {
+    result.groupAsync = (...args: any[]) =>
+      asyncWrapper(groupAsync(iterable, ...args)).map(fluentGroup as any);
+  }
+  if (partition) {
+    result.partition = (...args: any[]) =>
+      wrapper(partition(iterable, ...args)).map((part: any) => wrapper(part));
+  }
+  return result;
 }
