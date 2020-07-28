e1622b31c41344c53d06ec15e99e374168aae2e4
Thiago Santos <thiago.santos@maxmilhas.com.br>
2020-07-27 23:35:05 -0300

    refactoring combine

--- src/recipes/combine-recipe.ts
+++ src/recipes/combine-recipe.ts
@@ -1,6 +1,7 @@
 import { AnyIterable, Mapper } from 'augmentative-iterable';
 import { ResolverType } from '../types-internal';
 import { identity } from '../utils';
+import { getMapItems, getMapResult, getMapNN } from './combine-utils';
 
 export function combineRecipe(
   map: Function,
@@ -16,28 +17,16 @@ export function combineRecipe(
     keyB: Mapper<U, unknown>,
   ) {
     const m = new Map<unknown, U[]>();
-    return resolver(
-      forEach.call(iterable, (u: U) => {
-        const key = keyB(u);
-        let p = m.get(key);
-        if (!p) {
-          p = [] as U[];
-          m.set(key, p);
-        }
-        p.push(u);
-        return m;
-      }),
-      () =>
-        flatten.call(
-          filter.call(
-            map.call(itThis, (t: T) => {
-              const result = m.get(keyA(t));
-              return result ? result.map((u) => [t, u]) : undefined;
-            }),
-            identity,
-          ),
-        ),
+    return resolver(forEach.call(iterable, getMapItems(m, keyB)), () =>
+      flatten.call(
+        filter.call(map.call(itThis, getMapResult(m, keyA)), identity),
+      ),
     );
+  }
+  function combineNN<T, U>(itThis: AnyIterable<T>, iterable: AnyIterable<U>) {
+    const cache: U[] = [];
+    const mapped = map.call(itThis, (a: T) => [a, iterable]);
+    return flatten.call(mapped, getMapNN(cache, map));
   }
   return function <T, U>(
     this: AnyIterable<T>,
@@ -48,18 +37,8 @@ export function combineRecipe(
     if (!keyA === !!keyB) {
       throw new Error('Both key mapper must be informed, or no one');
     }
-    if (keyA) {
-      return innerJoin(this, iterable, keyA, keyB!);
-    }
-    const cache: U[] = [];
-    const mapped = map.call(this, (a: T) => [a, iterable]);
-    return flatten.call(mapped, ([a, bIt]: [T, AnyIterable<U>]) =>
-      cache.length > 0
-        ? map.call(cache, (b: U) => [a, b])
-        : map.call(bIt, (b: U) => {
-            cache.push(b);
-            return [a, b];
-          }),
-    );
+    return keyA
+      ? innerJoin(this, iterable, keyA, keyB!)
+      : combineNN(this, iterable);
   };
 }
