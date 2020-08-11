651155246cd4f6896bdef8b90e5c92410d6b0ddd
Thiago Santos <thiago.santos@maxmilhas.com.br>
2020-08-11 19:30:21 -0300

    updating for-emit-of

--- src/emitter/get-iterable-from-emitter.ts
+++ src/emitter/get-iterable-from-emitter.ts
@@ -1,58 +1,10 @@
 import { FluentEmitOptions } from '../types-base';
 import { EventEmitter } from 'events';
 import forEmitOf = require('for-emit-of');
-import fluent from '../fluent';
-
-const defaultOptions = { event: 'data', error: 'error', end: ['end', 'close'] };
 
 export function getIterableFromEmitter<T = any>(
   emitter: EventEmitter,
   options: FluentEmitOptions | undefined,
 ): AsyncIterable<T> {
-  // TODO: Remove all this wrapping after for-emit-of updates it's version to treat the case where a break can be made in the for await iterating over the async iterable generated
-  const treatedOptions = {
-    ...defaultOptions,
-    keepAlive: 1000,
-    ...options,
-  };
-
-  if (treatedOptions.end.indexOf('cancel') < 0) {
-    treatedOptions.end.push('cancel');
-  }
-
-  const customEmitter = new EventEmitter();
-  fluent([treatedOptions.event, treatedOptions.error, ...treatedOptions.end])
-    .distinct()
-    .forEach((event) =>
-      emitter.on(event, (data) => customEmitter.emit(event, data)),
-    );
-
-  const iterable = forEmitOf<T>(customEmitter, treatedOptions);
-
-  return {
-    [Symbol.asyncIterator]() {
-      const iterator = iterable[Symbol.asyncIterator]();
-      return {
-        async next() {
-          const nextValue = await iterator.next();
-
-          if (nextValue.done) {
-            customEmitter.emit('cancel');
-          }
-
-          return nextValue;
-        },
-        async return(value?: any) {
-          customEmitter.emit('cancel');
-          let done: any;
-          do {
-            const nextValue = await iterator.next();
-            done = nextValue.done;
-          } while (!done);
-
-          return { done: true, value };
-        },
-      };
-    },
-  };
+  return forEmitOf<T>(emitter, { ...options, keepAlive: 1000 });
 }
