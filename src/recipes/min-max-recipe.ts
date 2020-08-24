084d1c8c75f4735c6383a793385e72ed626d7b11
Thiago Santos <thiago.santos@maxmilhas.com.br>
2020-08-24 01:04:44 -0300

    fixing min max recipe duplicates

--- src/recipes/min-max-recipe.ts
+++ src/recipes/min-max-recipe.ts
@@ -7,53 +7,48 @@ type FunctionChecker = (f: Function, it: AnyIterable<any>) => boolean;
 type CheckAcceptance = (last: any, current: any) => boolean;
 
 function minMaxRecipe(
-  { filter, resolver, first, last }: MinMaxIngredients,
   accept: CheckAcceptance,
   lastChecker: FunctionChecker,
   firstChecker: FunctionChecker,
 ) {
-  return function <T>(this: any, mapper: any = identity): any {
-    let lastMapped: any;
-    let firstItem = true;
-    let lastItem: T | undefined;
-    const predicate = (c: T) =>
-      resolver(mapper(c), (mapped) => {
-        if (firstItem || accept(lastMapped, mapped)) {
-          firstItem = false;
-          lastMapped = mapped;
-          lastItem = c;
-          return true;
-        }
+  return function ({ filter, resolver, first, last }: MinMaxIngredients) {
+    return function <T>(this: any, mapper: any = identity): any {
+      let lastMapped: any;
+      let firstItem = true;
+      let lastItem: T | undefined;
+      const predicate = (c: T) =>
+        resolver(mapper(c), (mapped) => {
+          if (firstItem || accept(lastMapped, mapped)) {
+            firstItem = false;
+            lastMapped = mapped;
+            lastItem = c;
+            return true;
+          }
 
-        return lastMapped === mapped;
-      });
-    let operation: any;
-    if (lastChecker(mapper, this)) {
-      operation = last.call(filter.call(this, assureOrder(predicate)));
-    } else if (firstChecker(mapper, this)) {
-      operation = first.call(this, predicate);
-    } else {
-      operation = last.call(filter.call(this, predicate));
-    }
+          return lastMapped === mapped;
+        });
+      let operation: any;
+      if (lastChecker(mapper, this)) {
+        operation = last.call(filter.call(this, assureOrder(predicate)));
+      } else if (firstChecker(mapper, this)) {
+        operation = first.call(this, predicate);
+      } else {
+        operation = last.call(filter.call(this, predicate));
+      }
 
-    return resolver(operation, () => lastItem);
+      return resolver(operation, () => lastItem);
+    };
   };
 }
 
-export function maxRecipe(ingredients: MinMaxIngredients) {
-  return minMaxRecipe(
-    ingredients,
-    (last: any, current: any) => last < current,
-    isOrderAssured,
-    isDescendingOrderAssured,
-  );
-}
+export const maxRecipe = minMaxRecipe(
+  (last: any, current: any) => last < current,
+  isOrderAssured,
+  isDescendingOrderAssured,
+);
 
-export function minRecipe(ingredients: MinMaxIngredients) {
-  return minMaxRecipe(
-    ingredients,
-    (last: any, current: any) => last > current,
-    isDescendingOrderAssured,
-    isOrderAssured,
-  );
-}
+export const minRecipe = minMaxRecipe(
+  (last: any, current: any) => last > current,
+  isDescendingOrderAssured,
+  isOrderAssured,
+);
