1595d15b4cf9b29d320082990a74fc8bb4419a77
Thiago Santos <thiago.santos@maxmilhas.com.br>
2020-10-03 10:52:42 -0300

    fixing duplication again

--- src/utils.ts
+++ src/utils.ts
@@ -12,7 +12,7 @@ const valueTypes = ['string', 'number', 'boolean'];
  * Returns exactly the informed parameter
  * @param param The informed parameter to be returned
  */
-export function identity<T>(param: T): T {
+function identity<T>(param: T): T {
   return param;
 }
 
@@ -30,7 +30,7 @@ async function* promiseIterateAsync<T>(
  * @typeparam T the item type of the [[Iterable]]
  * @param a The async iterable
  */
-export function iterateAsync<T>(
+function iterateAsync<T>(
   a: AnyIterable<T> | PromiseLike<AnyIterable<T>>,
 ): AsyncIterable<T> {
   return ((a as any).then || (a as any)[Symbol.iterator]
@@ -43,7 +43,7 @@ export function iterateAsync<T>(
  * @typeparam T the item type of the internal [[Iterable/AsyncIterable]]
  * @param a The async iterable
  */
-export async function* iterateAllAsync<T>(a: AsyncIterable<AnyIterable<T>>) {
+async function* iterateAllAsync<T>(a: AsyncIterable<AnyIterable<T>>) {
   for await (const it of a) {
     yield* it;
   }
@@ -54,13 +54,13 @@ export async function* iterateAllAsync<T>(a: AsyncIterable<AnyIterable<T>>) {
  * @typeparam T the item type of the [[Iterable]]
  * @param a The iterable
  */
-export const iterate = identity;
+const iterate = identity;
 
 /**
  * Returns a function that always returns the informed value
  * @param value the constant value
  */
-export function constant<T>(value: T) {
+function constant<T>(value: T) {
   return function constantValue() {
     return value;
   };
@@ -71,7 +71,7 @@ export function constant<T>(value: T) {
  * @typeparam T the item type of the internal [[Iterable]]
  * @param a The iterable
  */
-export function* iterateAll<T>(a: Iterable<Iterable<T>>) {
+function* iterateAll<T>(a: Iterable<Iterable<T>>) {
   for (const it of a) {
     yield* it;
   }
@@ -81,7 +81,7 @@ export function* iterateAll<T>(a: Iterable<Iterable<T>>) {
  * Iterates over all owned properties of the given object
  * @param obj The object to iterate with
  */
-export function* iterateObjProps<T extends object>(obj: T): Iterable<keyof T> {
+function* iterateObjProps<T extends object>(obj: T): Iterable<keyof T> {
   for (const property in obj) {
     yield property;
   }
@@ -91,7 +91,7 @@ export function* iterateObjProps<T extends object>(obj: T): Iterable<keyof T> {
  * Iterates over all owned entries of given object
  * @param obj The object to iterate with
  */
-export function* iterateObjEntries<T extends object>(obj: T) {
+function* iterateObjEntries<T extends object>(obj: T) {
   for (const property of iterateObjProps(obj)) {
     yield [property, obj[property]];
   }
@@ -102,7 +102,7 @@ export function* iterateObjEntries<T extends object>(obj: T) {
  * @typeparam T the type of b
  * @param b the value for comparison
  */
-export function eq<T>(b: any) {
+function eq<T>(b: any) {
   return (a: T) => a === b;
 }
 
@@ -111,7 +111,7 @@ export function eq<T>(b: any) {
  * @typeparam T the type of b
  * @param b the value for comparison
  */
-export function gt<T>(b: any) {
+function gt<T>(b: any) {
   return (a: T) => a > b;
 }
 
@@ -120,7 +120,7 @@ export function gt<T>(b: any) {
  * @typeparam T the type of b
  * @param b the value for comparison
  */
-export function ge<T>(b: any) {
+function ge<T>(b: any) {
   return (a: T) => a >= b;
 }
 
@@ -129,7 +129,7 @@ export function ge<T>(b: any) {
  * @typeparam T the type of b
  * @param b the value for comparison
  */
-export function lt<T>(b: any) {
+function lt<T>(b: any) {
   return (a: T) => a < b;
 }
 
@@ -138,31 +138,31 @@ export function lt<T>(b: any) {
  * @typeparam T the type of b
  * @param b the value for comparison
  */
-export function le<T>(b: any) {
+function le<T>(b: any) {
   return (a: T) => a <= b;
 }
 
 /**
  * Provides an empty iterable
  */
-export function* empty(): Iterable<undefined> {}
+function* empty(): Iterable<undefined> {}
 
 /**
  * Provides an empty async iterable
  */
-export async function* emptyAsync(): AsyncIterable<undefined> {}
+async function* emptyAsync(): AsyncIterable<undefined> {}
 
 /**
  * Always returns true
  */
-export function truth(): boolean {
+function truth(): boolean {
   return true;
 }
 
 /**
  * Always returns false
  */
-export function falsity(): boolean {
+function falsity(): boolean {
   return false;
 }
 
@@ -171,7 +171,7 @@ export function falsity(): boolean {
  * @typeparam T the item type of the [[Predicate]]
  * @param predicate The predicate to be negated
  */
-export function negation<T>(predicate: Predicate<T>): Predicate<T> {
+function negation<T>(predicate: Predicate<T>): Predicate<T> {
   return (item: T) => !predicate(item);
 }
 
@@ -180,9 +180,7 @@ export function negation<T>(predicate: Predicate<T>): Predicate<T> {
  * @typeparam T the item type of the [[AsyncPredicate]]
  * @param predicate The async predicate to be negated
  */
-export function asyncNegation<T>(
-  predicate: AsyncPredicate<T>,
-): AsyncPredicate<T> {
+function asyncNegation<T>(predicate: AsyncPredicate<T>): AsyncPredicate<T> {
   return async (item: T) => !(await predicate(item));
 }
 
@@ -192,7 +190,7 @@ export function asyncNegation<T>(
  * @typeparam Value the type of the items of the value property
  * @param {Group} grp the [[Group]] to be converted
  */
-export function fluentGroup<Key, Value>(
+function fluentGroup<Key, Value>(
   grp: Group<Value, Key>,
 ): FluentGroup<Value, Key> {
   return {
@@ -204,7 +202,7 @@ export function fluentGroup<Key, Value>(
 /**
  * Returns an object to calculates incremental average/iterative means
  */
-export function getAverageStepper() {
+function getAverageStepper() {
   let avg = 0;
   let count = 0;
   const wrapper: AverageStepper = {
@@ -236,7 +234,7 @@ function getItemToAssure<
  *
  * @param f the function to assure order
  */
-export function assureOrder<
+function assureOrder<
   F extends Function | FluentIterable<any> | FluentAsyncIterable<any>
 >(f: F): F {
   const result = getItemToAssure(f);
@@ -256,7 +254,7 @@ export function assureOrder<
  *
  * @param f the function to assure order
  */
-export function assureOrderDescending<
+function assureOrderDescending<
   F extends Function | FluentIterable<any> | FluentAsyncIterable<any>
 >(f: F): F {
   const result = getItemToAssure(f);
@@ -286,3 +284,29 @@ export function desc<F>(f: F): F {
     (isValueType(f) ? { [valueTypeWrapper]: f } : f) as any,
   );
 }
+
+export {
+  assureOrder,
+  assureOrderDescending,
+  constant,
+  empty,
+  emptyAsync,
+  identity,
+  truth,
+  falsity,
+  negation,
+  asyncNegation,
+  fluentGroup,
+  getAverageStepper,
+  eq,
+  ge,
+  gt,
+  le,
+  lt,
+  iterateAsync,
+  iterateAllAsync,
+  iterate,
+  iterateAll,
+  iterateObjProps,
+  iterateObjEntries,
+};
