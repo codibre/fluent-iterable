9e6e601449e41b5e4350fe6de938cd8583ca71c7
Thiago Santos <thiago.santos@maxmilhas.com.br>
2021-01-31 14:31:19 -0300

    updating augmentative iterable and treating order assurance for map

--- src/recipes/augment-iterable-recipe.ts
+++ src/recipes/augment-iterable-recipe.ts
@@ -1,12 +1,28 @@
-import { AnyIterable } from 'augmentative-iterable';
-import { AnyMapper } from '../types-internal';
+import { AnyIterable, itClone } from 'augmentative-iterable';
+import { AnyMapper, orderAssured } from '../types-internal';
 import { prepare } from '../types-internal/prepare';
+import { assureOrder } from '../utils';
 
-export function augmentIterableRecipe(augmentIterableAsync: Function) {
+export function augmentIterableRecipe(
+  augmentIterable: Function,
+  clone?: boolean,
+) {
+  if (clone) {
+    return function <T>(
+      this: AnyIterable<T>,
+      action: AnyMapper<T>,
+    ): AnyIterable<T> {
+      return augmentIterable(
+        (this as any)[orderAssured] ? itClone(this) : this,
+        prepare(action),
+      );
+    };
+  }
+
   return function <T>(
     this: AnyIterable<T>,
     action: AnyMapper<T>,
   ): AnyIterable<T> {
-    return augmentIterableAsync(this, prepare(action));
+    return augmentIterable(this, prepare(action));
   };
 }
