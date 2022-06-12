19595a3f045ccb98c9d7de9ec63b915df5fd34ff
Thiago Santos <thiago.santos@maxmilhas.com.br>
2022-06-12 11:09:46 -0300

    chore: updating TAKE threshold for benchmark

--- test-benchmark/general-benchmark.spec.ts
+++ test-benchmark/general-benchmark.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { fluent }  from '../src';
+import { fluent } from '../src';
 import rxjs = require('rxjs');
 import rxjsOp = require('rxjs/operators');
 import { iterate } from 'iterare';
@@ -11,51 +11,51 @@ const FLAT_FACTOR = 10;
 const MULTIPLIER1 = 3;
 const MULTIPLIER2 = 2;
 const QUOTIENT = 5;
-const TAKE = 2000;
+const TAKE = 500;
 const benchmarkSuite = new Benchmark.Suite();
-function *interval(init, final) {
-  for(let i = init; i <= final; i++) {
+function* interval(init, final) {
+  for (let i = init; i <= final; i++) {
     yield i;
   }
 }
 function interval2(init, final) {
   const items: any[] = [];
-  for(let i = init; i <= final; i++) {
+  for (let i = init; i <= final; i++) {
     items.push(i);
   }
   return items;
 }
-function *map1(it) {
-   for (const x of it) {
-     yield x * MULTIPLIER1;
-   }
+function* map1(it) {
+  for (const x of it) {
+    yield x * MULTIPLIER1;
+  }
 }
-function *map2(it) {
-   for (const x of it) {
-     yield x * MULTIPLIER2;
-   }
+function* map2(it) {
+  for (const x of it) {
+    yield x * MULTIPLIER2;
+  }
 }
-function *filter1(it) {
-   for (const x of it) {
-     if (x % QUOTIENT === 0) {
-       yield x;
-     }
-   }
+function* filter1(it) {
+  for (const x of it) {
+    if (x % QUOTIENT === 0) {
+      yield x;
+    }
+  }
 }
-function *take1(it) {
-   let count = 0;
-   for (const x of it) {
-     count++;
-     if (count >= TAKE) {
-       break;
-     }
-     yield interval2(x, x + FLAT_FACTOR);
-   }
+function* take1(it) {
+  let count = 0;
+  for (const x of it) {
+    count++;
+    if (count >= TAKE) {
+      break;
+    }
+    yield interval2(x, x + FLAT_FACTOR);
+  }
 }
-function *map3(it) {
-   for (const x of it) {
-     yield interval2(x, x + FLAT_FACTOR);
-   }
+function* map3(it) {
+  for (const x of it) {
+    yield interval2(x, x + FLAT_FACTOR);
+  }
 }
 let log = '';
 
@@ -63,26 +63,26 @@ describe('General benchmark', () => {
   it('fluent should win', async () => {
     benchmarkSuite.add('fluent', () => {
       fluent(interval(1, ITEMS))
-          .map((x) => x * MULTIPLIER1)
-          .map((x) => x * MULTIPLIER2)
-          .filter((x) => x % QUOTIENT === 0)
-          .map((x) => interval2(x, x + FLAT_FACTOR))
-          .take(TAKE)
-          .forEach((x) => x.join(','));
-  }).add('array operation chain', () => {
+        .map((x) => x * MULTIPLIER1)
+        .map((x) => x * MULTIPLIER2)
+        .filter((x) => x % QUOTIENT === 0)
+        .map((x) => interval2(x, x + FLAT_FACTOR))
+        .take(TAKE)
+        .forEach((x) => x.join(','));
+    }).add('array operation chain', () => {
       interval2(1, ITEMS).map((x) => x * MULTIPLIER1)
-          .map((x) => x * MULTIPLIER2)
-          .filter((x) => x % QUOTIENT === 0)
-          .map((x) => interval2(x, x + FLAT_FACTOR))
-          .filter((_x, i) => i < TAKE)
-          .map((x) => x.join(','));
-  }).add('native iterable', () => {
+        .map((x) => x * MULTIPLIER2)
+        .filter((x) => x % QUOTIENT === 0)
+        .map((x) => interval2(x, x + FLAT_FACTOR))
+        .filter((_x, i) => i < TAKE)
+        .map((x) => x.join(','));
+    }).add('native iterable', () => {
       const it0 = interval2(1, ITEMS);
       const it = map3(take1(filter1(map2(map1(it0)))));
       for (const x of it) {
         x.join(',');
       }
-  }).add('rxjs', () => {
+    }).add('rxjs', () => {
       return rxjs.from(interval2(1, ITEMS))
         .pipe(
           rxjsOp.map((x) => x * MULTIPLIER1),
@@ -92,29 +92,29 @@ describe('General benchmark', () => {
           rxjsOp.take(TAKE),
           rxjsOp.map((x) => x.join(',')),
         ).toPromise();
-  }).add('iter-tools', () => {
-    return iterTools.execPipe(
-      interval(1, ITEMS),
-      iterTools.map((x) => x * MULTIPLIER1),
-      iterTools.map((x) => x * MULTIPLIER2),
-      iterTools.filter((x) => x % QUOTIENT === 0),
-      iterTools.map((x) => interval2(x, x + FLAT_FACTOR)),
-      iterTools.take(TAKE),
-      iterTools.forEach((x) => x.join(',')),
-    );
-  }).add('iterare', () => {
-    iterate(interval(1, ITEMS))
-      .map((x) => x * MULTIPLIER1)
-      .map((x) => x * MULTIPLIER2)
-      .filter((x) => x % QUOTIENT === 0)
-      .map((x) => interval2(x, x + FLAT_FACTOR))
-      .take(TAKE)
-      .forEach((x) => x.join(','));
-}).on('cycle', function(event) {
-    log += `${event.target}\n`;
-  }).on('complete', function(this: any) {
-    console.log(log);
-    expect(this.filter('fastest').map('name')).to.contains('fluent');
-  }).run();
-});
+    }).add('iter-tools', () => {
+      return iterTools.execPipe(
+        interval(1, ITEMS),
+        iterTools.map((x) => x * MULTIPLIER1),
+        iterTools.map((x) => x * MULTIPLIER2),
+        iterTools.filter((x) => x % QUOTIENT === 0),
+        iterTools.map((x) => interval2(x, x + FLAT_FACTOR)),
+        iterTools.take(TAKE),
+        iterTools.forEach((x) => x.join(',')),
+      );
+    }).add('iterare', () => {
+      iterate(interval(1, ITEMS))
+        .map((x) => x * MULTIPLIER1)
+        .map((x) => x * MULTIPLIER2)
+        .filter((x) => x % QUOTIENT === 0)
+        .map((x) => interval2(x, x + FLAT_FACTOR))
+        .take(TAKE)
+        .forEach((x) => x.join(','));
+    }).on('cycle', function (event) {
+      log += `${event.target}\n`;
+    }).on('complete', function (this: any) {
+      console.log(log);
+      expect(this.filter('fastest').map('name')).to.contains('fluent');
+    }).run();
+  });
 });
