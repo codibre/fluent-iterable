c586dc53b38a76cb16cc1affa551ae75adb96cb6
Thiago Oliveira Santos <tos.oliveira@gmail.com>
2023-01-15 11:03:43 -0300

    test: improving unwind test (#55)
    Co-authored-by: Thiago Santos <thiago.santos@maxmilhas.com.br>

--- test/unwind.spec.ts
+++ test/unwind.spec.ts
@@ -8,19 +8,19 @@ describe('unwind', () => {
         const payload = [
           {
             test: ['a', 'b', 'c'],
-            c: 1,
+            c: [1, 3],
             d: false,
             id: 1,
           },
           {
             test: ['b', 'c'],
-            c: 2,
+            c: [2, 3],
             d: true,
             id: 2,
           },
           {
             test: ['c', 'd'],
-            c: 2,
+            c: [2, 4],
             d: true,
             id: 3,
           },
@@ -29,31 +29,80 @@ describe('unwind', () => {
         const it = fluent(payload).unwind('test', 'c');
         const result = it.toArray();
 
-        expect(
-          result
-            .filter((x) => x.unwinded.test === 'a' && x.unwinded.c === 1)!
-            .map((x) => x.value.id),
-        ).to.be.eql([1]);
-        expect(
-          result
-            .filter((x) => x.unwinded.test === 'b' && x.unwinded.c === 1)!
-            .map((x) => x.value.id),
-        ).to.be.eql([1]);
-        expect(
-          result
-            .filter((x) => x.unwinded.test === 'b' && x.unwinded.c === 2)!
-            .map((x) => x.value.id),
-        ).to.be.eql([2]);
-        expect(
-          result
-            .filter((x) => x.unwinded.test === 'c' && x.unwinded.c === 2)!
-            .map((x) => x.value.id),
-        ).to.be.eql([2, 3]);
-        expect(
-          result
-            .filter((x) => x.unwinded.test === 'd' && x.unwinded.c === 2)!
-            .map((x) => x.value.id),
-        ).to.be.eql([3]);
+        expect(result).to.be.eql([
+          {
+            unwinded: { test: 'a', c: 1 },
+            value: { test: ['a', 'b', 'c'], c: [1, 3], d: false, id: 1 },
+          },
+          {
+            unwinded: { test: 'a', c: 3 },
+            value: { test: ['a', 'b', 'c'], c: [1, 3], d: false, id: 1 },
+          },
+          {
+            unwinded: { test: 'b', c: 1 },
+            value: { test: ['a', 'b', 'c'], c: [1, 3], d: false, id: 1 },
+          },
+          {
+            unwinded: { test: 'b', c: 3 },
+            value: { test: ['a', 'b', 'c'], c: [1, 3], d: false, id: 1 },
+          },
+          {
+            unwinded: { test: 'c', c: 1 },
+            value: { test: ['a', 'b', 'c'], c: [1, 3], d: false, id: 1 },
+          },
+          {
+            unwinded: { test: 'c', c: 3 },
+            value: { test: ['a', 'b', 'c'], c: [1, 3], d: false, id: 1 },
+          },
+          {
+            unwinded: { test: 'b', c: 2 },
+            value: { test: ['b', 'c'], c: [2, 3], d: true, id: 2 },
+          },
+          {
+            unwinded: { test: 'b', c: 3 },
+            value: { test: ['b', 'c'], c: [2, 3], d: true, id: 2 },
+          },
+          {
+            unwinded: { test: 'c', c: 2 },
+            value: { test: ['b', 'c'], c: [2, 3], d: true, id: 2 },
+          },
+          {
+            unwinded: { test: 'c', c: 3 },
+            value: { test: ['b', 'c'], c: [2, 3], d: true, id: 2 },
+          },
+          {
+            unwinded: { test: 'c', c: 2 },
+            value: { test: ['c', 'd'], c: [2, 4], d: true, id: 3 },
+          },
+          {
+            unwinded: { test: 'c', c: 4 },
+            value: { test: ['c', 'd'], c: [2, 4], d: true, id: 3 },
+          },
+          {
+            unwinded: { test: 'd', c: 2 },
+            value: { test: ['c', 'd'], c: [2, 4], d: true, id: 3 },
+          },
+          {
+            unwinded: { test: 'd', c: 4 },
+            value: { test: ['c', 'd'], c: [2, 4], d: true, id: 3 },
+          },
+        ]);
+        expect(result[0].value).to.be.eq(payload[0]);
+        expect(result[1].value).to.be.eq(payload[0]);
+        expect(result[2].value).to.be.eq(payload[0]);
+        expect(result[3].value).to.be.eq(payload[0]);
+        expect(result[4].value).to.be.eq(payload[0]);
+        expect(result[5].value).to.be.eq(payload[0]);
+
+        expect(result[6].value).to.be.eq(payload[1]);
+        expect(result[7].value).to.be.eq(payload[1]);
+        expect(result[8].value).to.be.eq(payload[1]);
+        expect(result[9].value).to.be.eq(payload[1]);
+
+        expect(result[10].value).to.be.eq(payload[2]);
+        expect(result[11].value).to.be.eq(payload[2]);
+        expect(result[12].value).to.be.eq(payload[2]);
+        expect(result[13].value).to.be.eq(payload[2]);
       });
     });
     describe('async', () => {
