31686e93ba8d87309420ad9c20931302fb4bc46c
Thiago Santos <tos.oliveira@gmail.com>
2023-03-20 11:14:21 -0300

    fix: fixing mapping os symbol and numbers

--- src/types-internal/prepare.ts
+++ src/types-internal/prepare.ts
@@ -1,12 +1,14 @@
 import { valueTypeWrapper } from './string-wrapper';
 import { AnyMapper, FunctionAnyMapper } from '../types/any-mapper';
 
+const fieldTypes = new Set(['string', 'number', 'symbol']);
+
 export function prepare<T>(predicate: AnyMapper<T>): FunctionAnyMapper<T> {
   if (predicate.hasOwnProperty(valueTypeWrapper)) {
     return (t: any) => t[(predicate as any)[valueTypeWrapper]];
   }
 
-  return typeof predicate === 'string'
-    ? (t: T) => t[predicate]
+  return fieldTypes.has(typeof predicate)
+    ? (t: T) => t[predicate as keyof T]
     : (predicate as FunctionAnyMapper<T>);
 }
