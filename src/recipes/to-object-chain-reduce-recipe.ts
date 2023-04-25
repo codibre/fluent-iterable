292fb2a3c5b7be548bab9932df95a46ab74923eb
Thiago Santos <tos.oliveira@gmail.com>
2023-04-24 21:27:51 -0300

    git pushfeat: accepting function mapping for to object chain

--- src/recipes/to-object-chain-reduce-recipe.ts
+++ src/recipes/to-object-chain-reduce-recipe.ts
@@ -1,25 +1,41 @@
+import { FunctionAnyMapper } from './../types/any-mapper';
 import { AnyIterable } from 'augmentative-iterable';
 import { AsyncReducer } from '../types';
 import { ToObjectChainIngredients } from './ingredients';
+import { prepare } from '../types-internal/prepare';
+
+function unwindAndGroup(
+  it: AnyIterable<any>,
+  ing: ToObjectChainIngredients,
+  key: FunctionAnyMapper<any>,
+) {
+  it = ing.flatten.call(it, (x: any) => {
+    const current = key(x);
+    return typeof current !== 'string' &&
+      typeof current[Symbol.iterator] === 'function'
+      ? ing.map.call(current, (item) => [item, x])
+      : [[current, x]];
+  });
+  return ing.group.call(it, (x: any) => x[0]);
+}
 
 function toObjectNode<T, R>(
   it: AnyIterable<any>,
-  keys: string[],
+  keys: FunctionAnyMapper<any>[],
   index: number,
   ing: ToObjectChainIngredients,
   reducer: AsyncReducer<T, R>,
   initial: () => R,
 ): any {
   const key = keys[index];
-  it = ing.unwind.call(it, key);
-  it = ing.group.call(it, (x: any) => x.unwinded[key]);
+  it = unwindAndGroup(it, ing, key);
   return ing.toObject.call(
     it,
     'key',
     index < keys.length - 1
       ? (subIt: any) =>
           toObjectNode(
-            ing.map.call(subIt.values, (x: any) => x.value),
+            ing.map.call(subIt.values, (x: any) => x[1]),
             keys,
             index + 1,
             ing,
@@ -28,7 +44,7 @@ function toObjectNode<T, R>(
           )
       : (subIt: any) =>
           ing.reduce.call(
-            ing.map.call(subIt.values, (x: any) => x.value),
+            ing.map.call(subIt.values, (x: any) => x[1]),
             reducer,
             initial(),
           ),
@@ -42,8 +58,9 @@ export function toObjectChainReduceRecipe(ing: ToObjectChainIngredients) {
     reducer: AsyncReducer<T, R>,
     ...keys: string[]
   ) {
+    const prepared = keys.map((x) => prepare(x));
     return keys.length === 0
       ? ing.reduce.call(this, reducer, initial())
-      : toObjectNode(this, keys, 0, ing, reducer, initial);
+      : toObjectNode(this, prepared, 0, ing, reducer, initial);
   };
 }
--- src/sync/to-object-chain.ts
+++ src/sync/to-object-chain.ts
@@ -1,14 +1,14 @@
 import { toObjectChainRecipe } from '../recipes';
 import { basic } from './basic-ingredients';
+import { flatten } from './flatten';
 import { group } from './group';
 import { reduce } from './reduce';
 import { toObject } from './to-object';
-import { unwind } from './unwind';
 
 export const toObjectChain = toObjectChainRecipe({
   ...basic,
   group,
   toObject,
-  unwind,
+  flatten,
   reduce,
 });
