2cf03988b689eaae10db8c02ea10193eeb657de0
Thiago Santos <thiago.santos@maxmilhas.com.br>
2023-11-06 07:20:22 -0300

    feat: creating decrement function

--- src/interval.ts
+++ src/interval.ts
@@ -4,6 +4,7 @@ import { FluentIterable } from './types';
 function internalInterval(
   fromInclusive: number | undefined,
   count: number | undefined,
+  step: number,
 ): Iterable<number> {
   const from = fromInclusive || 0;
   let offset = 0;
@@ -12,7 +13,7 @@ function internalInterval(
       next() {
         const done = count !== undefined && offset >= count;
         const value = from + offset;
-        offset++;
+        offset += step;
         return {
           value,
           done,
@@ -42,5 +43,27 @@ export function interval(
   fromInclusive?: number,
   count?: number,
 ): FluentIterable<number> {
-  return fluent(internalInterval(fromInclusive, count));
+  return fluent(internalInterval(fromInclusive, count, 1));
+}
+
+/**
+ * Generates a `count` long sequential integer interval starting from `fromInclusive`.
+ *
+ * * The interval ends in negative infinity if `count` is not specified.
+ *
+ * Examples:
+ *
+ * 1. `decrement(5, 3)` generates an interval of `[5, 4, 3]`
+ * 2. `decrement(5)` generates an interval of `[5, 4, 3, ...]`
+ * 3. `decrement()` generates an interval of `[0, -1, -2, ...]`
+ * 4. `decrement(1).take(10).forEach(console.log)` prints all numbers between 1 and -9.
+ * @param fromInclusive Specifies the start of the interval. Defaults to zero.
+ * @param count Specifies the length of the interval. The interval provides numbers indefinitely if omitted.
+ * @returns The iterable of numbers in the interval.
+ */
+export function decrement(
+  fromInclusive?: number,
+  count?: number,
+): FluentIterable<number> {
+  return fluent(internalInterval(fromInclusive, count, -1));
 }
